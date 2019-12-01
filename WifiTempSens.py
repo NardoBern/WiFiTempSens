ccef6d6a845b289aacbd3972b4bc311239ce7e3f
Nardo Bern <nardo.bern@gmail.com>
2019-12-01 11:16:40 +0100

    Implementazione funzionalità
    Aggiunta gestione sveglia del sensore, invio del messaggio di GetTemperature, invio del messaggio torna a dormire.
    Connessione con database data_store.db

--- WifiTempSens.py
+++ WifiTempSens.py
@@ -12,6 +12,7 @@ site.addsitedir(sys.path[0]+'/class')
 site.addsitedir(sys.path[0]+'/modules')
 from temperatureSensor import temperatureSensor
 from cadenziatore import cadenziatore
+from db_manager import database_engine
 ###############################
 #### IMPOSTAZIONE COSTANTI ####
 ###############################
@@ -27,23 +28,42 @@ def oneMinFunction():
         UDP_PORT = sensTempCucina.UdpPort
         UDP_IP = sensTempCucina.IpAdr
         try:
+            #### Invio il messaggio di sveglia ####
             MESSAGE = sensTempCucina.wakeUpMsg    
             sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
             sock.bind(("",UDP_PORT))
             sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
             sock.settimeout(10.0)
-            print("Aspetto dei dati")
+            print("Aspetto il id sensore")
             data, addr = sock.recvfrom(1024)
             print(data)
+            if data == sensTempCucina.sensId:
+                print ("Id confermato - acquisisco temperatura")
+                MESSAGE = "GetTemp"
+                sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+                sock.bind(("",UDP_PORT))
+                sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+                data, addr = sock.recvfrom(1024)
+                print(data)
+                sensTempCucina.valore = float(data)
+                print(addr[0])
+                sensTempCucina.resetErrore()
+            else:
+                print ("Id non confermato")
+                sensTempCucina.gestioneErrore()
         except socket.timeout:
             sock.settimeout(None)
             print("Time-out di comunicazione")
-        MESSAGE = "STOP"
+            sensTempCucina.gestioneErrore()
+        #### Invio il messaggio di STOP ####
+        MESSAGE = sensTempCucina.sleepMsg
         sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         sock.bind(("",UDP_PORT))
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
     elif selettore.indice == 15:
         print 'Qui salviamo tutto...'
+        data_store.scriviTemperatura(sensTempCucina.valore*100,"cucina")
+        data_store.salva_dati()
         print 'Resetto il indice'
         selettore.indice = 0
     print 'INTERRUPT ad 1 Min.'
@@ -56,8 +76,10 @@ def oneMinFunction():
 try:
     print "Inizializzazione sensore temperatura cucina"
     sensTempCucina = temperatureSensor()
-    sensTempCucina.wakeUpMsg = "sveglia"
-    sensTempCucina.IpAdr = "10.0.1.108"
+    sensTempCucina.wakeUpMsg = "cucinaSveglia"
+    sensTempCucina.sleepMsg = "cucinaDormi"
+    sensTempCucina.IpAdr = "10.0.1.10"
+    sensTempCucina.sensId = "cucina"
 except Exception,e:
     print ("Errore inizializzazione sensore temperatura cucina: ",e)
 
@@ -70,6 +92,15 @@ try:
     selettore.inizializzazione()
 except Exception,e:
     print 'ERRORE NELLA INIZIALIZZAZIONE DEL CADENZIATORE'
+    print e
+#############################################################
+#### INIZIALIZZAZIONE CONNESSIONA AL DATABASE DATA_STORE ####
+#############################################################
+try:
+    print 'INIZIALIZZAZIONE CONNESSIONE AL DATABASE DATA_STORE'
+    data_store = database_engine('/home/pi/dbFolder/data_store.db')
+except Exception,e:
+    print 'ERRORE NELLA INIZIALIZZAZIONE DEL DATABASE DEI TREND'
     print e
 ####################################################
 #### INIZIALIZZAZIONE TIMER INTERRUPT AD 1 MIN. ####
--- class/temperatureSensor.py
+++ class/temperatureSensor.py
@@ -8,5 +8,19 @@ class temperatureSensor:
         self.UdpPort    = 64123
         self.nFailConn  = 0
         self.wakeUpMsg  = ""
+        self.sleepMsg   = ""
         self.sensId     = ""
-        self.maxTimeOut = 3 
+        self.maxTimeOut = 3
+        self.numOfErr   = 0
+        self.commErr    = False
+    
+    def gestioneErrore(self):
+        self.numOfErr = self.numOfErr + 1
+        if self.numOfErr >= self.maxTimeOut:
+            self.commErr = True
+    
+    def resetErrore(self):
+        if self.numOfErr != 0:
+            self.numOfErr = 0
+        if self.commErr:
+            self.commErr = False
